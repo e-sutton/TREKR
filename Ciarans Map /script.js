543eb509a926077fd081f5c3f23fc60c9cc95968
Ciaran <ciaran426@yahoo.co.uk>
2015-10-16 21:20:42 +0100

    Ciaran map funtionality

--- Ciarans Map /script.js
+++ Ciarans Map /script.js
@@ -26,7 +26,7 @@ function addKmlLayer(map) {
 }
 
 function drawMarkers(map) {
-    var image = "hike.gif";
+    var image = "hike.png";
 
     var marker = new google.maps.Marker({
         icon: image,
@@ -43,7 +43,7 @@ function drawMarkers(map) {
       '<div id="bodyContent">'+
       '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
       'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
       'south west of the nearest large town, Alice Springs; 450&#160;km '+
       '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
       'features of the Uluru - Kata Tjuta National Park. Uluru is '+
@@ -67,3 +67,48 @@ function drawMarkers(map) {
   });
 
 }
+
+
+///SUPPOSED TO GET ELEVATION AT LOCATION....//
+function addElevationService(){
+    //create elevation service
+    elevationService = new google.maps.ElevationService();
+    
+    //Add a listener for double click and call getELevation at location
+    google.maps.event.addEventListener(map,'click',getElevation);
+    
+}
+
+function getElevation(event){
+    var locations = [];
+    
+    var infowindow = new google.maps.InfoWindow();
+    //Find clicked location and add to array
+    
+    var userClickLocation = even.latlng;
+    locations.push(userClickLocation);
+    
+    //Create locationElevationReequest object using array value
+    var positionalRequest = {
+        'locations' : locations
+    }
+    //send the location request
+    elevationServive.getElevationForLocations(
+        positionalRequest, function (results, status){
+            if(status == google.maps.ElevationStatus.OK){
+                //Retrieve first result
+                if(results[0]){
+                    //Open info window with elevation at position
+                    infowindow.setContent("Elecation at this point is: "+ Math.round(results[0].elevation) + " metres ");
+                    inforwindow.setPosition(userClickLocation);
+                    infowindow.open(map);
+                }else{
+                    alert("No results found.")
+                }
+            }else{
+                alert("Elevation service failed due to: " + status);
+            }
+        });
+}
+            
+  
